8f9348147fa8e84275b7e5b314f813b2edc4643e
Krzysztof Rak <krzysztof.rak@billennium.pl>
2017-12-05 15:08:52 +0100

    Utworzenie projektu

---
+++ Portal aukcyjny/Portal aukcyjny/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portal_aukcyjny.Startup))]
+namespace Portal_aukcyjny
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
